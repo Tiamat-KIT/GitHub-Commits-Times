d412659b3f1e48caa1ebf560d72325a370d15289
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-04-29 17:29:25 +0000

    わかりやすいように修正

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ repo = "yumemi-homework-app-retry"
 token = os.environ['GITHUB_TOKEN']
 
 # ヘッダーにトークンを追加
-headers = {
-    "Authorization": f"token {token}"
-}
+headers = {"Authorization": f"token {token}"}
 
 # ブランチごとの時間の割合と開発時間を計算
 branch_times = defaultdict(int)
@@ -24,34 +22,43 @@ branch_times = defaultdict(int)
 # ページネーションを使用して全てのコミットを取得
 page = 1
 while True:
-    response = requests.get(API_URL.format(owner=owner, repo=repo), headers=headers, params={"page": page})
-    commits = response.json()
-    if not commits:
-        break
+  response = requests.get(API_URL.format(owner=owner, repo=repo),
+                          headers=headers,
+                          params={"page": page})
+  commits = response.json()
+  if not commits:
+    break
 
-    # 最初のコミットの日時を開始日として設定
-    if page == 1:
-        start_date = datetime.datetime.strptime(commits[0]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
+  # 最初のコミットの日時を開始日として設定
+  if page == 1:
+    end_date = datetime.datetime.strptime(
+        commits[0]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
 
-    # 最後のコミットの日時を終了日として設定
-    end_date = datetime.datetime.strptime(commits[-1]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
+  # 最後のコミットの日時を終了日として設定
+  start_date = datetime.datetime.strptime(
+      commits[-1]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
 
-    # ブランチごとの時間の割合と開発時間を計算
-    for commit in commits:
-        # ここでは、コミットメッセージの最初の行をブランチ名として扱っています。
-        # 実際のコミットメッセージの形式に応じてパターンを調整してください。
-        branch = commit['commit']['message'].split('\n')[0]
-        branch_times[branch] += 1
+  # ブランチごとの時間の割合と開発時間を計算
+  for commit in commits:
+    # ここでは、コミットメッセージの最初の行をブランチ名として扱っています。
+    # 実際のコミットメッセージの形式に応じてパターンを調整してください。
+    branch = commit['commit']['message'].split('\n')[0]
+    branch_times[branch] += 1
 
-    page += 1
+  page += 1
 
 # 開発にかかった時間を計算
 total_time = end_date - start_date
 
+# 合計開発時間の表示方法を修正
+total_days = total_time.days
+total_hours = total_time.seconds // 3600
+total_minutes = (total_time.seconds // 60) % 60
+
 # 結果の表示
-print(f"Start date: {start_date}")
-print(f"End date: {end_date}")
-print(f"Total development time: {total_time}")
-print("Branch times and development times:")
-for branch, time in branch_times.items():
-    print(f"{branch}: {time}")
+print(f"最初のコミット：{start_date.strftime('%Y/%m/%d %H:%M:%S')}")
+print(f"最後のコミット：{end_date.strftime('%Y/%m/%d %H:%M:%S')}")
+print(f"合計開発時間：{total_days}日 {total_hours}時間 {total_minutes}分")
+# print("各ブランチでの開発時間")
+# for branch, time in branch_times.items():
+#  print(f"- [{branch}] : {time}時間")
